07d70e43fad7bbd7f9c33e3c39a37317e9e5ae64
Nrn Badar-Uugan <macbook@192.168.1.7>
2026-04-15 20:26:32 +0800

    coming back

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -37,7 +37,7 @@ export async function GET(req: NextRequest) {
             },
           }
         : {}),
-      ...(filter === "discount"
+      ...(filter === "featured"
         ? { discountEnabled: true }
         : {}),
     },
